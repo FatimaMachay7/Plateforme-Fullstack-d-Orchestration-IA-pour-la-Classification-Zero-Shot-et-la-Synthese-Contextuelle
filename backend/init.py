bd57423164f332ef049e5eb8af45860dbc54e4dc
fatimamachay-pixel <fatimamachay5@email.com>
2025-12-05 17:37:27 +0100

    finaliser la partie frontend

--- backend/init.py
+++ backend/init.py
@@ -1,26 +1,26 @@
-import os
-import requests
-from dotenv import load_dotenv
-from fastapi import FastAPI
-app=FastAPI()
+# import os
+# import requests
+# from dotenv import load_dotenv
+# from fastapi import FastAPI
+# app=FastAPI()
 
-load_dotenv()
-API_URL = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-mnli"
-headers = {
-    "Authorization": f"Bearer {os.environ['HF_TOKEN']}",
-}
+# load_dotenv()
+# API_URL = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-mnli"
+# headers = {
+#     "Authorization": f"Bearer {os.environ['HF_TOKEN']}",
+# }
 
-def query(text :str):
-    response = requests.post(API_URL, headers=headers, json={
-    "inputs": text,
-    "parameters": {"candidate_labels": ["refund", "legal", "faq"]},
-})
-    return response.json()
+# def query(text :str):
+#     response = requests.post(API_URL, headers=headers, json={
+#     "inputs": text,
+#     "parameters": {"candidate_labels": ["refund", "legal", "faq"]},
+# })
+#     return response.json()
 
-output = query("Je suis hahah")
-print (output)
+# output = query("Je suis hahah")
+# print (output)
 
-@app.post('/huggingface')
-def hugginface(text:str):
-    resu= query(text)
-    return resu
+# @app.post('/huggingface')
+# def hugginface(text:str):
+#     resu= query(text)
+#     return resu
